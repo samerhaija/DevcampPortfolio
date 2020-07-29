fc26936ce6d5ade3e43da554984a6a998832badd
Samer Haija <samer.haija@gmail.com>
2020-07-28 19:20:10 -0700

    Added nav bar and added default header content

---
+++ app/controllers/concerns/default_page_content.rb
@@ -0,0 +1,12 @@
+module DefaultPageContent
+  extend ActiveSupport::Concern
+
+  included do
+    before_action :set_page_defaults
+  end
+
+  def set_page_defaults
+    @page_title = "Devcamp Portfolio | My Porfolio Website"
+    @seo_keywords = "Samer Haija portfolio"
+  end
+end
