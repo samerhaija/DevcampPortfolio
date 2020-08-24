87e487dd6c72fad6ced2f907804b83a786976bc3
Samer Haija <samer.haija@gmail.com>
2020-08-23 21:42:43 -0700

    Implemented authorization engine for portolio items and blogs

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,6 +1,7 @@
 class PortfoliosController < ApplicationController
   before_action :set_portfolio_item, only: [:edit, :update, :show, :destroy]
   layout "portfolio"
+  access all: [:show, :index], user: {except: [:destroy, :new, :create, :update, :edit]}, site_admin: :all
   
   def index
     @portfolio_items = Portfolio.all
@@ -41,7 +42,6 @@ def update
   end
 
   def show
-    binding.pry
   end
 
   def destroy
